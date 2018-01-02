a21c7430b1281d517dfdf48da8c132e9422833a6
grundiss <grundiss@gmail.com>
2018-01-02 14:38:29 +0300

    Main banner

--- src/client/pages/main/index.js
+++ src/client/pages/main/index.js
@@ -7,10 +7,10 @@ import style from "./styles.css";
 export default () => (
   <Page>
     <div className={style.banner}>
-      <img src="/assets/lilenko.png" className={style.bannerImg} />
+      <img src="/assets/lilenko-main.png" className={style.bannerImg} />
       <div className={style.bannerText}>
-        <div className={style.bannerTextTop}>Жили у бабуси</div>
-        <div>два веселых гуся</div>
+        <div className={style.bannerTextTop}>Практика краудфандинга</div>
+        <div>изнутри и снаружи</div>
       </div>
     </div>
     Главная страница...
