7491fedeea7e5ac3bfa56df7f9ef8ebcdef8a3d3
grundiss <grundiss@gmail.com>
2018-01-20 15:01:55 +0300

    Ya.Metruka

--- src/server/html.jsx
+++ src/server/html.jsx
@@ -11,7 +11,53 @@ const Html = ({ App, title = "lilenko.ru", bundleName }) => (
       <link rel="stylesheet" href={`/static/css/${bundleName}.bundle.css`} />
     </head>
     <body>
+      {process.env.NODE_ENV === "production" && (
+        <React.Fragment>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function (d, w, c) {
+                  (w[c] = w[c] || []).push(function() {
+                      try {
+                          w.yaCounter47376385 = new Ya.Metrika2({
+                              id:47376385,
+                              clickmap:true,
+                              trackLinks:true,
+                              accurateTrackBounce:true,
+                              webvisor:true
+                          });
+                      } catch(e) { }
+                  });
+          
+                  var n = d.getElementsByTagName("script")[0],
+                      s = d.createElement("script"),
+                      f = function () { n.parentNode.insertBefore(s, n); };
+                  s.type = "text/javascript";
+                  s.async = true;
+                  s.src = "https://mc.yandex.ru/metrika/tag.js";
+          
+                  if (w.opera == "[object Opera]") {
+                      d.addEventListener("DOMContentLoaded", f, false);
+                  } else { f(); }
+                })(document, window, "yandex_metrika_callbacks2");
+            `,
+            }}
+          />
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/47376385"
+                style="position:absolute; left:-9999px;"
+                alt=""
+              />
+            </div>
+          </noscript>
+        </React.Fragment>
+      )}
+
       <div id="app" dangerouslySetInnerHTML={{ __html: ReactDOMServer.renderToString(<App />) }} />
+
       <script src="/static/js/common.bundle.js" />
       <script src={`/static/js/${bundleName}.bundle.js`} />
     </body>
